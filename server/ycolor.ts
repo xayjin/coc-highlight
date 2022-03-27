0d548292e2dd7134bba2bd4d7bc8cd5057498f76
4415u <yajin@debian-4415u.com>
2022-03-27 22:31:55 +0800

    路径错误，修改

--- server/ycolor.ts
+++ server/ycolor.ts
@@ -1,9 +1,12 @@
 import * as fs from 'fs';
+import path from 'path'
 function getcolor(): JSON | null {
-	if (fs.existsSync('./ycolor.json')) //判断是否存在此文件
+
+	  let file = path.resolve(__dirname, 'ycolor.json')
+	if (fs.existsSync(file)) //判断是否存在此文件
 	{
 		//读取文件内容，并转化为Json对象
-		let userBugsJson = JSON.parse(fs.readFileSync("./ycolor.json", "utf8"));
+		let userBugsJson = JSON.parse(fs.readFileSync(file, "utf8"));
 		//获取Json里key为data的数据
 		return userBugsJson;
 	}
